14cf5e172be3ae63ca02635523ec2c91a412c666
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-03-17 10:15:30 +0700

    jshint semicolon

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-const express = require('express')
-const app = express()
-const books = require('./db')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const books = require('./db');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
 app.listen(3000, () => {
-    console.log('Start server at port 3000.')
-})
+    console.log('Start server at port 3000.');
+});
 
 app.get('/books', (req, res) => {
-    res.json(books)
-})
+    res.json(books);
+});
 
 app.get('/books/:id', (req, res) => {
-    res.json(books.find(book => book.id === req.params.id))
-})
+    res.json(books.find(book => book.id === req.params.id));
+});
 
 app.post('/books', (req, res) => {
-    books.push(req.body)
-    res.status(201).json(req.body)
-})
+    books.push(req.body);
+    res.status(201).json(req.body);
+});
 
 app.put('/books/:id', (req, res) => {
-    const updateIndex = books.findIndex(book => book.id === req.params.id)
-    res.json(Object.assign(books[updateIndex], req.body))
-})
+    const updateIndex = books.findIndex(book => book.id === req.params.id);
+    res.json(Object.assign(books[updateIndex], req.body));
+});
 
 app.delete('/books/:id', (req, res) => {
-    const deletedIndex = books.findIndex(book => book.id === req.params.id)
-    books.splice(deletedIndex, 1)
-    res.status(204).send()
-})
+    const deletedIndex = books.findIndex(book => book.id === req.params.id);
+    books.splice(deletedIndex, 1);
+    res.status(204).send();
+});
